7c0f4518762d95be18856b192b3725e6cfe2d283
Radek <radek.rogalski88@gmail.com>
2022-10-03 16:14:40 +0200

    minor changes

--- event_feed.py
+++ event_feed.py
@@ -2,15 +2,25 @@ from fastapi import WebSocket, WebSocketDisconnect
 import schemas
 
 
+class UnknownHandler(Exception):
+    pass
+
+
 class EventFeed:
+    def __init__(self) -> None:
+        self.handlers = [self.ready, self.fire]
+
     async def __call__(self, context: WebSocket) -> None:
         await context.accept()
         try:
             while True:
                 payload = await context.receive_json()
                 message = schemas.Message(**payload)
-                # temporary solution
+                # temporary solution?
                 handler = getattr(self, message.event)
+                if handler not in self.handlers:
+                    return
+
                 await handler(context, message.data)
         except WebSocketDisconnect:
             await self.disconnect(context)
@@ -21,10 +31,6 @@ class EventFeed:
 
     async def fire(self, context: WebSocket, data):
         # task #5 / frontend #1
-        pass
-
-    async def start(self, context: WebSocket, data):
-        # task #3
         pass
 
     async def disconnect(self, context: WebSocket):
