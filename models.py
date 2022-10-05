a4b8fdbb90001e9e832a7f1b7a92d8427c5f388d
siejo <kotelek55@gmail.com>
2022-10-05 23:11:45 +0200

    still testing how branch works

---
+++ models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+
+class Player(Base):
+    __tablename__ = "player"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String)
+    wins = Column(Integer)
+    loses = Column(Integer)
+    warships = Column(list)
+    last_seen = Column(datetime)
+
+   # owner = relationship("User", back_populates="items")
+
+class Warship(Base):
+    __tablename__="warship"
+    id = Column(Integer, primary_key=True, index=True )
+    position = Column(_Position)
+    length = Column(Integer)
+    orientation = Column(Orientation)
+
+class Position(Base):
+    __tablename__="position"
+    id = Column(Integer, primary_key=True, index=True )
+    x = Column(Integer)
+    y = Column(Integer)
+
+class Orientation(Base):
+    __tablename__="orientation"
+    id = Column(Integer, primary_key=True, index=True )
+    vertical = Column(Integer)
+    horizontal = Column(Integer)
+
+
