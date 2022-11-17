c214ccbcc96f13114190dba00b7eec61b3aea99a
toja@example.com <toja@example.com>
2022-11-17 12:09:48 +0100

    djskadjsa

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from fastapi import Depends, FastAPI, HTTPException, status
 import websocket
 from fastapi.middleware import Middleware
@@ -65,34 +66,36 @@ websocket_middleware = [
 app_websocket: FastAPI = FastAPI(
     middleware=websocket_middleware)
 
+
+# zmienmy nawe tego, to juz nie bedzie tylko kolejka, ale cala "gra"
+# oprocz dodawania osob do kolejki tutaj powinnismy obslugiwac oddawanie strzalow
+# "shoot" z sesji przeniesc tutaj
 class GameQueue:
     def __init__(self) -> None:
         self.queue = []
+        self.game_sessions: list[GameSession] = []
 
     def enqueue(self, conn):
-        # conn.player.id
-        # ..nickname
         self.queue.append(conn)
         if len(self.queue) > 1:
-            # wyciagnac dwoch ostatnich graczy z kolejki
-            # stworzyc nowa sesje z tych dwoch graczy
-            # session.start()
-            pass
+            player_a, player_b = self.queue[:2]
+            self.queue = self.queue[2:]
+            game_session = GameSession(self, player_a, player_b)
+            self.game_sessions.append(game_session)
+            game_session.start()
 
-class GameSession:
-    def __init__(self, player_a: PlayerConnection, player_b: PlayerConnection) -> None:
-        self.player_a = player_a
-        self.player_b = player_b
-        # zapisywac kto w tym momencie ma ruch
+    def dequeue(self, conn):
 
         pass
 
-    def start(self):
-        # wyslac do obydwu uzytkownikow ten sam komunikat:
-        # { "status": "GAME_STARTED" }
-        pass
+    def shoot(self, conn: PlayerConnection, x: int, y: int) -> bool:
 
-    def shoot(self, x, y):
+        for session in self.game_sessions:
+            if session.contains(conn):
+                session.shoot(x,y)
+
+
+
         # czy jakikolwiek statek przeciwnika lezy w x, y
         # wyslac do uzytkownika który strzelil callback ze trafil
         # lub nie trafil
@@ -102,14 +105,55 @@ class GameSession:
         # i wtedy przciwnik.send({"status": "LOSE"})
         # przeciwnik.lose + 1
         # (a do przeciwnika czy dostal)
+
+
+class GameSession:
+    def __init__(self, ally: PlayerConnection, enemy: PlayerConnection) -> None:
+        self.ally: PlayerConnection = ally
+        self.enemy: PlayerConnection = enemy
+
+        self.now_moves = self.ally
+
+    def start(self):
+        self.ally.callback(WebSocketEvent.START, self.enemy.player.nickname)
+        self.enemy.callback(WebSocketEvent.START, self.ally.player.nickname)
+    
+    def shoot (self, x, y):
+
+        for warship in warships:
+            if warship.x == x and warship.y == y:
+                session1.hit()
         pass
+
+    def hit(self):
+        self.ally.callback(WebSocketEvent.HIT, { "wasHit": True })
+        #self.enemy.callback(WebSocketEvent.GOT_HIT, )
+
+    def constains(self, player: PlayerConnection):
+        if self.now_moves == player:
+            return True
+        return False
+
+
+class GameStatus(Enum):
+    UNKNOWN = 0
+    WIN = 1
+    LOSE = 2
+    ENEMY_DISCONNECTED = 3
+
+
+class WebSocketEvent(Enum):
+    UNKNOWN = 0
+    START = 1
+    HIT = 2
+    GOT_HIT = 3
 
 
 game_queue = GameQueue()
 
 
 async def connect(db: Session, conn: PlayerConnection, data):
-    await conn.callback("init", conn.player.warships)
+    await conn.callback("init", "hi")
 
 
 async def disconnect(db: Session, conn: PlayerConnection, data):
@@ -118,18 +162,20 @@ async def disconnect(db: Session, conn: PlayerConnection, data):
     # { "status": "ENEMY_DISCONNECTED" }
     pass
 
+
 async def shoot(db: Session, conn: PlayerConnection, data):
     # data zawiera to: { x: 0, y: 2 }
     # sprawdzic czy uzytkownik jest w tym momencie w sesji gry
     # jesli tak sprwaidzic czy teraz jego ruch
     # strzeclic w pole przciwnika
-    # session.shoot(x, y)
+    # session.shoot(conn, x, y)
     pass
 
+
 async def ready(db: Session, conn: PlayerConnection, data):
+    # update statków gracza jesli ułożenie się zmieniło
     game_queue.enqueue(conn)
-    print(game_queue.queue)
-    
+
 
 app_websocket.add_api_websocket_route('/', websocket.register_events([
     connect,
